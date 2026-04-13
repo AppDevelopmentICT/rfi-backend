4aa5fb9216d5bc38604c9a6dca482a1cb672bbe4
VannWasHere <ateen1203@gmail.com>
2026-04-13 14:53:09 +0700

    fix: script on windows

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 if [ -d "venv" ]; then
-    source venv/bin/activate
+    source venv/Scripts/activate
 else
     python3 -m venv venv
-    source venv/bin/activate
+    source venv/Scripts/activate
     pip install -r requirements.txt
 fi
 pip install -r requirements.txt --quiet
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn app.main:app --host 0.0.0.0 --port 1254 --reload
